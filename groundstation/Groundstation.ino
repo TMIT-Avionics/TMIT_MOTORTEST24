36a5f96e173ba9da77ff5ab1833b2d08922e6221
D-Coder-42 <137105924+D-Coder-42@users.noreply.github.com>
2024-09-09 20:21:11 +0530

    Updated Ignition FSM
    Added FSM which includes sending RYLR data packet

--- groundstation/Groundstation.ino
+++ groundstation/Groundstation.ino
@@ -3,16 +3,77 @@
 
 #define RYLR_RXD 7
 #define RYLR_TXD 6
+const String RYLR_ADD = "50"; //set to reciever RYLR address
+
+#define ARM_SWITCH 2
+#define LAUNCH_SWITCH 3
 
 SoftwareSerial RYLR(RYLR_RXD, RYLR_TXD);
 
 File logFile;
 
-void setup() {
-  
+typedef enum {SAFE, ARMED, LAUNCHED} STATUS;
+STATUS CURRENT_STATE = SAFE;
+String message, packet;
+
+void stateInput()
+{
+  switch(CURRENT_STATE)
+  {
+    case SAFE:
+      if (digitalRead(ARM_SWITCH) == HIGH && digitalRead(LAUNCH_SWITCH) == LOW)
+      {
+        CURRENT_STATE = ARMED;
+        delay(500); //debouncing delay
+        message = "ARMON";
+        packet = String("AT+SEND=")+RYLR_ADD+","+String(message.length())+","+message;
+        Serial.println("CURRENT STATE = ARMED");
+        RYLR.println(packet);
+        delay(100); // for transmission of data
+      }
+      break;
+    case ARMED:
+      if (digitalRead(ARM_SWITCH) == LOW)
+      {
+        CURRENT_STATE = SAFE;
+        delay(500); //debouncing delay
+        message = "SAFE";
+        packet = String("AT+SEND=")+RYLR_ADD+","+String(message.length())+","+message;
+        Serial.println("CURRENT STATE = SAFE");
+        RYLR.println(packet);
+        delay(100); // for transmission of data
+      }
+      else if (digitalRead(LAUNCH_SWITCH) == HIGH)
+      {
+        CURRENT_STATE = LAUNCHED;
+        delay(500); //debouncing delay
+        message = "LAUNCH";
+        packet = String("AT+SEND=")+RYLR_ADD+","+String(message.length())+","+message;
+        Serial.println("CURRENT STATE = LAUNCHED");
+        RYLR.println(packet);
+        delay(100); // for transmission of data
+      }
+      break;
+
+    case LAUNCHED:
+      break;
+  }
 }
 
-void loop() {
-
+void setup() 
+{
+  Serial.begin(9600);
+  RYLR.begin(57600);
+  if(CURRENT_STATE == SAFE)
+  {
+    message = "SAFE";
+    packet = String("AT+SEND=")+RYLR_ADD+","+String(message.length())+","+message;
+    Serial.println("CURRENT STATE = SAFE");
+    RYLR.println(packet);
+  }
 }
 
+void loop() 
+{
+  stateInput();
+}
